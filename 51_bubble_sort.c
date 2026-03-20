79463d0ba26763663e33305c0b2ff25b7950a172
rohit290106 <rohit290106@gmail.com>
2026-03-20 11:16:45 +0530

    final bubble sort

--- 51_bubble_sort.c
+++ 51_bubble_sort.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-void bubble_sort(int a[], int n)
+void bubble_sort(int array[], int n)
 {
     int temp;
     int swapped = 0;
 
     for (int j = 1; j < n; j++)
     {
+        swapped=0;
         printf("pass %d\n", j);
 
         for (int i = 0; i < n - j; i++)
         {
-            if (a[i] > a[i + 1])
+            if (array[i] > array[i + 1])
             {
-                temp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = temp;
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
                 swapped++;
             }
         }
@@ -28,25 +29,25 @@ void bubble_sort(int a[], int n)
     }
 }
 
-void traverse(int a[], int n)
+void traverse(int array[], int n)
 {
     for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
+        printf("%d ", array[i]);
     printf("\n");
 }
 
 int main()
 {
-    int a[] = {1, 45, 6, 9, 3, 8, 2, 5, 11, 19};
-    int n = sizeof(a) / sizeof(a[0]);
+    int array[] = {1, 45, 6, 9, 3, 8, 2, 5, 11, 19};
+    int n = sizeof(array) / sizeof(array[0]);
 
     printf("The array:\n");
-    traverse(a, n);
+    traverse(array, n);
 
-    bubble_sort(a, n);
+    bubble_sort(array, n);
 
     printf("The bubble sorted array:\n");
-    traverse(a, n);
+    traverse(array, n);
 
     return 0;
 }
