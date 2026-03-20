528496dad4cb5594dd1972e59757c42851f80224
rohit290106 <rohit290106@gmail.com>
2026-03-20 15:44:15 +0530

    Insertion sort

--- 51_bubble_sort.c
+++ 51_bubble_sort.c
@@ -32,6 +32,7 @@ void traverse(int a[], int n)
 {
     for (int i = 0; i < n; i++)
         printf("%d ", a[i]);
+        
     printf("\n");
 }
 
--- 53_insertion.sort.c
+++ 53_insertion.sort.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 
-void insertion_short(int a[],int n){
-    int temp;
-    int swapped;
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n-1;j++){
-            if(a[i]>a[i+1]){
-                temp=a[i];
-                a[i]=a[i+1];
-                a[i+1]=temp;
-            }
+void insertion_sort(int a[], int n)
+{
+    int key;
+    int j;
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        key = a[i];
+        j = i - 1;
+
+        while (a[j] > key && j >= 0)
+        {
+            a[j + 1] = a[j];
+            j--;
         }
+
+        a[j + 1] = key;
     }
 }
-int main(){
-    int arr[]={2,3,4,1,9,6,8,5,7};
-    int size=sizeof(arr)/sizeof(arr[0]);
 
+void traverse(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+        
+    printf("\n");
+}
+
+int main()
+{
+    int array[] = {1, 45, 6, 9, 3, 8, 2, 5, 11, 19};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    printf("The array:\n");
+    traverse(array, size);
+
+    insertion_sort(array, size);
+
+    printf("The insertion sorted array:\n");
+    traverse(array, size);
+
+    return 0;
 }
